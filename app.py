63ff03582439592a3ad61702d14d570da9f12f84
Maryna Velychynska <marien5@ukr.net>
2023-02-07 23:13:35 +0100

    app.py

--- app.py
+++ app.py
@@ -1,36 +1,46 @@
-
-import requests
-
-response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-data = response.json()
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 import csv
 
-with open('rates.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile)
- 
-    currency_list = [code, ask, bid]
+from flask import Flask, render_template, request
+import csv
+
+app = Flask(__name__)
+
+def open_csv():
+    rates = {}
+    with open('rates.csv') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        for row in reader:
+            currency = row['currency']
+            code = row['code']
+            bid = float(row['bid'])
+            ask = float(row['ask'])
+            rates[code] = (currency, bid, ask)
+    return rates
 
 @app.route("/currency", methods=["GET", "POST"])
 def currency():
-  if request.method == "POST":
-      data = request.form
-      sum = data.get('sum')
-      code = data.get('code') 
-  
-  for i in currency_list:
-    
-    if i == ask:
-      return  sum * ask
-    if i == bid:
-      return sum * bid
+    rates = open_csv()
 
+    if request.method == "POST":
+      code = request.form['code']
+      sum = float(request.form['sum'])
+      action = request.form['action']
 
-  return render_template("Currency_calculator.html")
+    if code in rates:
+     rates[code] = [code, sum, action]
+    if action == "buy":
+     result = sum * ask
+    elif action == "sell":
+     result = sum * bid
+        
+
+    return render_template("Currency_calculator.html", result=result)
+
+   
 
 if __name__ == '__main__':
     app.run(debug=True)
